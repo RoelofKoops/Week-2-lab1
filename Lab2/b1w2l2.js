e9e55cbc2901eafcc32a3faaf58ba97fdfb32cc2
roel0Fzo <99055522@mydavinci.nl>
2019-09-09 14:20:38 +0200

    l1 gemaakt

---
+++ Lab2/b1w2l2.js
@@ -0,0 +1,22 @@
+alert("Hallo welkom op deze website, hier volgen wat vragen"); 
+
+document.write(prompt ('voer je Voornaam hieronder in')); 
+
+document.write("<br />"); 
+
+document.write(prompt ('voer je achternaam hieronder in')); 
+
+document.write("<br />"); 
+
+document.write(prompt ('Voer je leeftijd hieronder in'));
+
+document.write("<br />"); 
+
+
+document.write("<br />"); 
+
+
+
+
+
+
---
+++ b1w2l1/b1w2l1.js
@@ -0,0 +1,20 @@
+alert("Hallo welkom op deze website, hier volgen wat vragen"); //Geeft een pop-up met de tekst die hier in het geel staat
+
+document.write(prompt ('voer je Voornaam hieronder in')); //geeft een pop-Up met vraag en antwoordbalkje
+
+document.write("<br />"); //zorgt ervoor dat er een enter tussen de antwoorden staat
+
+document.write(prompt ('voer je achternaam hieronder in'));  //geeft een pop-Up met vraag en antwoordbalkje
+
+document.write("<br />"); //zorgt ervoor dat er een enter tussen de antwoorden staat
+
+document.write(prompt ('Voer je leeftijd hieronder in')); //geeft een pop-Up met vraag en antwoordbalkje
+
+document.write("<br />"); //zorgt ervoor dat er een enter tussen de antwoorden staat
+
+
+
+
+
+
+
